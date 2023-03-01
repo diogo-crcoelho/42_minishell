93a2f51ea269438d95e50b1398b94bed0d18f39c
dcarvalh <dcarvalh@student.42lisboa.com>
2023-03-01 18:38:28 +0000

    fixed some shit

--- srcs/libs/strs/ft_split.c
+++ srcs/libs/strs/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: dcarvalh <dcarvalh@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 19:09:06 by dcarvalh          #+#    #+#             */
-/*   Updated: 2023/02/09 17:29:01 by dcarvalh         ###   ########.fr       */
+/*   Updated: 2023/03/01 18:37:17 by dcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,10 +29,10 @@ char	**rec_split(char **splited, int words, char c, const char *s)
         while (*s && *s != c)
             *word++ = *s++;
     }
-    if (*s == c)
+    if (word)
         splited = rec_split(splited, words + 1, c, s);
     else if (!*s)
-        splited = ft_calloc(sizeof(char *) * (words + 2));
+        splited = ft_calloc(sizeof(char *) * (words + 1));
     splited[words] = word - i;
     return (splited);
 }
--- srcs/signals/signals.c
+++ srcs/signals/signals.c
@@ -6,7 +6,7 @@
 /*   By: dcarvalh <dcarvalh@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 11:54:53 by dcarvalh          #+#    #+#             */
-/*   Updated: 2023/02/20 13:20:49 by dcarvalh         ###   ########.fr       */
+/*   Updated: 2023/02/28 17:35:08 by dcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 void ctrl_c(int sig)
 {
 	(void)sig;
-	printf("\n");
+	write(2,"^C\n", 3);
 	rl_replace_line("", 1);
 	rl_on_new_line();
 	rl_redisplay();
@@ -32,6 +32,7 @@ void ctrl_bs(int sig)
 
 void signals_hand(void)
 {
+	rl_catch_signals = 0;
 	signal(SIGINT, ctrl_c);
 	signal(SIGQUIT, ctrl_bs);
 }
